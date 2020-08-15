b5fd8933d0539612988bfb0335d41c83ca1f9414
Frank Choukouali <fchokouali2014@gmail.com>
2020-08-15 12:21:11 -0400

    week 6 Making decision

--- week7/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ week7/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -24,8 +24,21 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         // Update the screen with the dice roll
+        val luckyNumber = 4
+        var message=""
+        when (diceRoll) {
+            //if the luckuNumber == rollResult print You won
+            luckyNumber -> message="You won!"
+            //if the 1 == rollResult print So sorry! You rolled a 1. Try again!
+            1 -> message="So sorry! You rolled a 1. Try again!"
+            2 -> message="Sadly, you rolled a 2. Try again!"
+            3 -> message="Unfortunately, you rolled a 3. Try again!"
+            4 -> message="No luck! You rolled a 4. Try again!"
+            5 -> message="Don't cry! You rolled a 5. Try again!"
+            6 -> message="Apologies! you rolled a 6. Try again!"
+        }
         val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        resultTextView.text = message
     }
 }
 
